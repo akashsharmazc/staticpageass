dc68a52febc9cf618dee92f31b4f0fa91612fa03
Akash Sharma <akash.sharmazc@gmail.com>
2020-10-09 04:14:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function expand()
+{
+    console.log("here")
+var a=document.getElementById("Body-1").style.display="flex"
+var b=document.getElementById("Searchbar").style.display="block"
+var c=document.getElementById("Button-div").style.display="flex"
+var d=document.getElementById("Addtocart").style.display="block"
+var e=document.getElementById("Buyonamazon").style.display="block"
+var e=document.getElementById("COLOUR")
+var f=document.getElementById("GRIDS").style.display="grid"
+document.getElementById("expander").style.left="251px"
+
+}
